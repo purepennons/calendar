cd81a80bce3ac7f2693e3f947dd8fd652d550c04
Chiahao Lin <y80811@gmail.com>
2019-12-27 00:14:50 +0800

    feat: implement `CalendarLogic` component

--- src/components/CalendarLogic/CalendarLogic.jsx
+++ src/components/CalendarLogic/CalendarLogic.jsx
@@ -1,25 +1,89 @@
 import { Component } from 'react'
 import PropTypes from 'prop-types'
 import { noop } from 'lodash'
+import { setDate, setMonth, setYear, isEqual as isDateEqual } from 'date-fns'
 
-import { getDateList } from './utils'
+import { getDateListPair, getMonthListPair, getYearListPair } from './utils'
 
 class CalendarLogic extends Component {
   constructor(props) {
     super(props)
 
+    this.isUnmounted = false
     this.state = this.getDefaultState()
   }
 
-  getDefaultState = () => {
+  componentDidUpdate(prevProps) {
     const { value } = this.props
+    if (!isDateEqual(value, prevProps.value)) {
+      this.setState(this.getDefaultState())
+    }
+  }
+
+  componentWillUnmount() {
+    this.isUnmounted = true
+  }
+
+  getDefaultState = () => {
+    const { value, defaultValue } = this.props
     return {
-      internalDate: value || new Date(),
+      internalDate: value || defaultValue || new Date(),
     }
   }
 
   getRenderProps = () => {
-    return
+    const { value: date, yearPeriod: period } = this.props
+    const { internalDate } = this.state
+
+    return {
+      date,
+      internalDate,
+      year: {
+        ...getYearListPair(internalDate, { period }),
+      },
+      month: {
+        ...getMonthListPair(),
+      },
+      day: {
+        ...getDateListPair(internalDate),
+      },
+      handlers: {
+        setYear: this.setYear,
+        setMonth: this.setMonth,
+        setDate: this.setDate,
+        onSelect: this.onSelect,
+      },
+    }
+  }
+
+  safeSetState = (...args) => {
+    if (this.isUnmounted) {
+      return
+    }
+
+    this.setState(...args)
+  }
+
+  setYear = value => {
+    this.safeSetState(({ internalDate }) => ({
+      internalDate: setYear(internalDate, value),
+    }))
+  }
+
+  setMonth = value => {
+    this.safeSetState(({ internalDate }) => ({
+      internalDate: setMonth(internalDate, value),
+    }))
+  }
+
+  setDate = value => {
+    this.safeSetState(({ internalDate }) => ({
+      internalDate: setDate(internalDate, value),
+    }))
+  }
+
+  onSelect = () => {
+    this.props.onChange(this.state.internalDate)
   }
 
   render() {
@@ -29,11 +93,14 @@ class CalendarLogic extends Component {
 
 CalendarLogic.propTypes = {
   children: PropTypes.func.isRequired,
+  defaultValue: PropTypes.instanceOf(Date),
   value: PropTypes.instanceOf(Date),
+  yearPeriod: PropTypes.number,
   onChange: PropTypes.func,
 }
 
 CalendarLogic.defaultProps = {
+  yearPeriod: 10,
   onChange: noop,
 }
 
