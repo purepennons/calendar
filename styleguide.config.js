3d291a476fe9e660c01ed618728fb95ea90b7553
Chiahao Lin <y80811@gmail.com>
2019-12-30 00:44:17 +0800

    chore($styleguidist): ignore no document components

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+  skipComponentsWithoutExample: true,
   components: 'src/components/**/[A-Z]*.{js,jsx}',
   styleguideComponents: {
     Wrapper: path.join(__dirname, 'src/styleguidist/Wrapper'),
